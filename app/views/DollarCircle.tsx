a465c4cf50eea0de2765b5c373276a1d23e0f0c3
Ting Liu <mr.tingliu@gmail.com>
2023-12-23 00:20:03 -0800

    feat: add animations on information circle

--- app/views/DollarCircle.tsx
+++ app/views/DollarCircle.tsx
@@ -1,6 +1,10 @@
 export const DollarCircle: React.FC<{className?: string}> = ({className=""}) => {
     return (
-      <button className={`bg-green-600 rounded-full h-8 w-8 flex items-center justify-center shadow-md ${className}`}>
+      <button 
+        className={
+          `bg-green-600 rounded-full h-8 w-8 flex items-center 
+          justify-center shadow-md ${className}`
+      }>
         <span className="text-gray-300 text-lg font-medium">$</span>
       </button>
     );
--- app/views/InformationCircle.tsx
+++ app/views/InformationCircle.tsx
@@ -1,6 +1,11 @@
 export const InformationCircle: React.FC<{className?: string}> = ({className=""}) => {
     return (
-      <button className={`bg-gray-400 rounded-full h-4 w-4 flex items-center justify-center shadow-sm ${className}`}>
+      <button 
+        className={
+          `bg-gray-400 rounded-full h-4 w-4 flex items-center
+          justify-center shadow-sm hover:bg-gray-500 focus:ring 
+          transform active:scale-y-75 transition-transform ${className}`
+      }>
         <span className="text-gray-200 text-xs font-medium font-serif">i</span>
       </button>
     );
